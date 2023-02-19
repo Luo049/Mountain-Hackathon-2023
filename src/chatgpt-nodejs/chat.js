782bba9c1c1e460a484162c01259b4217dfd564e
Vito Lau <Vito@Vitos-MBP.hitronhub.home>
2023-02-19 02:53:57 -0800

    hi

--- src/chatgpt-nodejs/chat.js
+++ src/chatgpt-nodejs/chat.js
@@ -1,19 +1,33 @@
+// import axios from 'axios'
+const http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+const cors = require("cors") //cross-origin resource sharing
+const axios = require('axios')
+const url = require('url')
+var app = express();
+var resdata = []
 
+
+const path = require('path')
+const PORT = process.env.PORT || 5010;
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+//app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    // apiKey: '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',
+    apiKey: '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'
 });
 const openai = new OpenAIApi(configuration);
-
-
-function inputPass(input, mood){
-    responseGenerator(input, mood)
-
-}
 
 
 async function responseGenerator (input, mood) {
@@ -42,19 +56,35 @@ async function oppositeMood (moodInput) {
     return completion.data.choices[0].text
 }
 
+
+async function getTranslation(URL){
+    const res = await axios.get(URL);
+    // console.log(res)
+    return res
+}
+
 // export default Chat
-
-(async () => {
-    input = "i want food"
-    mood = "angry"
-    response = (await responseGenerator(input, mood))
+app.post('/chatrecvm', async (req, res) => {
+	let emotion = req.body.emotion
+	let txt = req.body.textString
+	
+	console.log("Passthru: ", emotion, txt)
+    response = (await responseGenerator(txt, emotion))
+    let URL = "https://translate-server.herokuapp.com/combine/" + response
+    let translation = (await getTranslation(URL))
     console.log(response)
-})()
+    console.log(translation)
+	
+});
 
-// while(1){ 
+app.get('/chatrecvm', (req,res) => {
+	res.json("NULL");
+})
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+});
 
-//     (async () => {
-//         response = (await responseGenerator(input, mood))
-//         console.log(response)
-//     })()
-// }
+app.use((req, res) => {
+    res.status(404);
+});
+
