0a7717943cd356a1017dbf829032ed4b0f37e651
Vito Lau <Vito@d207-023-187-108.wireless.sfu.ca>
2023-02-18 20:01:38 -0800

    chat

---
+++ public/chat.js
@@ -0,0 +1,59 @@
+
+module.export = {responseGenerator}
+
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+
+function inputPass(input, mood){
+    responseGenerator(input, mood)
+
+}
+
+
+async function responseGenerator (input, mood) {
+    mood = oppositeMood(mood)
+    let inputMessage = "write a response message to" + input + "in a" + mood + "tone"
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: inputMessage,
+        max_tokens: 1000,
+    });
+    // console.log(completion.data.choices[0].text);
+    // getReturnMessage(completion.data.choices[0].text)
+    return completion.data.choices[0].text
+}
+
+
+async function oppositeMood (moodInput) {
+    let opposite = ("what is the opposite of" + moodInput)
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: opposite,
+        max_tokens: 1000,
+    });
+    // console.log(completion.data.choices[0].text);
+    // getReturnMessage(completion.data.choices[0].text)
+    return completion.data.choices[0].text
+}
+
+
+(async () => {
+    input = "i want food"
+    mood = "angry"
+    response = (await responseGenerator(input, mood))
+    console.log(response)
+})()
+
+// while(1){ 
+
+//     (async () => {
+//         response = (await responseGenerator(input, mood))
+//         console.log(response)
+//     })()
+// }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { responseGenerator } from './chatgpt-nodejs/chat.js';
 function App() {
+  
+
+  // var input = prompt("Type in a message: ");
+  // var mood = prompt("What is your mood? ");
+
+
+  // (async () => {
+  //   x = (await responseGenerator(input, mood))
+  //   console.log(x)
+  // })()
+
+
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +30,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
